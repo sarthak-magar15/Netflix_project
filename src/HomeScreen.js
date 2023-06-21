a5d82acbfbe9503c0eb56972d20b92fa57ce6dd8
sarthak-magar15 <magarsarthak15@gmail.com>
2023-06-21 21:26:10 +0530

    start of netflix clone

---
+++ src/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (<div className='homeScreen'>
+    {/* Nav bar */}
+
+    {/* Banner */}
+
+    {/* Rows */}
+    
+  </div>)
+}
+
+export default HomeScreen
