d69dd5f0ebd32badfc65e00eab382cd5f8f45ee3
sarthak-magar15 <magarsarthak15@gmail.com>
2023-06-25 19:06:42 +0530

    Completed redux implementation, userSlice, store.js complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen"; 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
-
+import { auth } from './firebase';
 
 function App() {
   const user = null; 
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) =>{
+      if(userAuth){
+        //login
+        console.log(userAuth);
+      }else{
+        //Logged Out
+      }
+    });
+
+    return unsubscribe; 
+  }, []);
   
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+// compat packages are API compatible with namespaced code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAIcsb9D8VOUlLRIJj8CWNlTBjmd4zT7lU",
     authDomain: "netflix-clone-ffe67.firebaseapp.com",
@@ -6,3 +11,11 @@ const firebaseConfig = {
     messagingSenderId: "396805738101",
     appId: "1:396805738101:web:06082f1d55ea7157ac323c"
   };
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig); 
+  const db = firebaseApp.firestore(); 
+  const auth = firebase.auth(); 
+
+  export {auth}; 
+  export default db; 
+  
