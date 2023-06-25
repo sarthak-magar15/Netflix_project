274374c8599b56eccfd2f13c9dbfd24dc4e720ba
sarthak-magar15 <magarsarthak15@gmail.com>
2023-06-25 22:33:44 +0530

    Added Profile Screen, Moving on to plans screen

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import HomeScreen from "./screens/HomeScreen"; 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { auth } from './firebase';
+import { login, logout, selectUser } from './features/userSlice';
+import store from './app/store';
 
 function App() {
-  const user = null; 
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth) =>{
-      if(userAuth){
-        //login
-        console.log(userAuth);
-      }else{
-        //Logged Out
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
       }
     });
 
-    return unsubscribe; 
-  }, []);
-  
+    return () => unsubscribe();
+  }, [dispatch]); // Include 'dispatch' in the dependency array
+
   return (
     <div className="app">
       <Router>
         {!user ? (
-          <LoginScreen/>
+          <LoginScreen />
         ) : (
           <Routes>
-          <Route exact path="/" element={<HomeScreen/>}></Route>
-        </Routes>
+            <Route path="/profile" element={<ProfileScreen/>}/>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
         )}
-        
       </Router>
     </div>
   );
 }
 
-export default App;
+const RootApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default RootApp;
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './Nav.css'
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
 
   const [show, handleShow] = useState(false); 
+  const navigate = useNavigate()
 
   const transitionNavBar = () => {
     if(window.scrollY > 100){
@@ -23,11 +25,15 @@ function Nav() {
     <div className={`nav ${show && 'nav_black'}`}>
         <div className='nav_contents'>
 
-        <img className='nav_logo' 
+        <img 
+        onClick={() => navigate("/")}
+        className='nav_logo' 
         src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
         alt="" />
 
-        <img className='nav_avatar'
+        <img 
+        onClick={() => navigate("/profile")}
+        className='nav_avatar'
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
         alt="" />
 
